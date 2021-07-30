accfea9cfe2fe851263af74ab05432d5806f424a
Os Cabuloso <88209987+os-cabuloso-oficial@users.noreply.github.com>
2021-07-30 16:23:31 -0300

    Create ui.py

---
+++ data/ui.py
@@ -0,0 +1,43 @@
+import os, sys, time
+from colorama import Fore, Style
+from random import choice
+clean=('cls' if os.name == 'nt' else 'clear')
+C= "\033[97;1m"
+G = "\033[92;1m"
+P = "\033[1;35m"
+result='''{P}\n________           _________       ___.         .__                      
+\_____  \   ______ \_   ___ \_____ \_ |__  __ __|  |   ____  __________  
+ /   |   \ /  ___/ /    \  \/\__  \ | __ \|  |  \  |  /  _ \/  ___/  _ \ 
+/    |    \\___ \  \     \____/ __ \| \_\ \  |  /  |_(  <_> )___ (  <_> )
+\_______  /____  >  \______  (____  /___  /____/|____/\____/____  >____/ 
+        \/     \/          \/     \/    \/                      \/       '''
+def banner():
+    cor = [Fore.RED]
+    banner = r"""
+====================
+☯️ - Painel Privado da Os Cabuloso! ☯️
+===================="""
+    n = 0
+    for char in banner:
+        sys.stdout.write(f"{choice(cor)}{char}{Style.RESET_ALL}")
+        sys.stdout.flush()
+        n +=1
+        time.sleep(0.007)
+
+def clear():
+	os.system(clean)
+
+def menu(ms0):
+	clear();banner();print(result+ms0)
+	return input(f'\n{C}===> {G}')
+
+def error():
+	text='Caractere(s) não reconhecido(s)';clear();banner();print(f'\n{C}====================\n[{P}Error!{C}] {text}\n====================');time.sleep(3)
+	
+def dialog1():
+	clear();banner();print(result)
+	return input(f'\n{C}===> {G}')
+
+def dialog2(msg):
+	clear();banner()
+	return input(f'{result}\n{C}====================\n{msg}\n====================\n{C}{G}Deseja fazer uma nova consulta?\n{C}[{G}1{C}] Sim\n[{G}2{C}] Nao\n===> {G}')
