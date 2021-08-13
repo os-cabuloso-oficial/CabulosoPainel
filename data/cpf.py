0e525658c3e71480889aa49985c185072a6a0223
Os Cabuloso <88209987+os-cabuloso-oficial@users.noreply.github.com>
2021-08-13 01:46:10 -0300

    Update cpf.py

--- data/cpf.py
+++ data/cpf.py
@@ -5,6 +5,7 @@ def consultar():
 	while (Sair==False):
 		cpf = ui.dialog1()
 		r = requests.get(f'https://dualityapi.xyz/apis/flex_7/Consultas%20Privadas/HTML/cpf.php?consulta={cpf}')
+		if 'A Consulta' in r:
 			msg='A consulta está funcionando normalmente, porém o CPF não foi encontrado.'
 		else:
 			msg=r.replace('''"''', '').replace(',','\n').replace('}', '').replace('{', '')
